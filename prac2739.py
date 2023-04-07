25b0153b1643d20b68951c80bce3653cf983d98c
thankYun <rlaehddbs2238@gmail.com>
2023-04-07 15:14:33 +0900

    namechange

---
+++ prac2739.py
@@ -0,0 +1,15 @@
+# N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 
+# 출력 형식에 맞춰서 출력하면 된다.
+
+# N 인풋 후 프린트 *1~9
+
+insertNum=int(input())
+print(insertNum*1)
+print(insertNum*2)
+print(insertNum*3)
+print(insertNum*4)
+print(insertNum*5)
+print(insertNum*6)
+print(insertNum*7)
+print(insertNum*8)
+print(insertNum*9)
