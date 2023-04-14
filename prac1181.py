5245cf6bad8004e6e92fe13ee4c26e92aa599226
thankYun <rlaehddbs2238@gmail.com>
2023-04-14 14:12:26 +0900

    우엉 우어엉

--- prac1181.py
+++ prac1181.py
@@ -3,16 +3,19 @@ import sys
 N=int(sys.stdin.readline())
 List=[]
 for i in range(N):
-    Z=input()
-    if Z in List:
-        pass
-    else:
-        for j in range (len(List)):
-            if len(List[j])>len(Z)>len(List[j-1]):
-                List.insert(j,Z)
+    Z=sys.stdin.readline
+    if Z not in List:
+        if len(List[i])>len(Z):
+            List.insert(0,Z)
+        elif len(List[i+1])>=len(Z)>len(List[i]):
+            List.insert(i,Z)
+        elif len(List[i+1])<len(Z):
+            List.insert(i-1,Z)
+        else:
+            List.append(Z)
 
     
 
-
+print(List)
 # for i in range(len(List)):
 #     print(List[i])
