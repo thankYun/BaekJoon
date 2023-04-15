07fdd32f11623872e3cfae1b41fe24cf83d3728d
ThankYun <rlaehddbs2238@gmail.com>
2023-04-15 16:57:10 +0900

    2week

---
+++ wwweeeek2/prac1920.py
@@ -0,0 +1,32 @@
+import sys
+
+N= int(sys.stdin.readline())
+Nlist=list(map(int,sys.stdin.readline().split()))
+M= int(sys.stdin.readline())
+Mlist=list(map(int,sys.stdin.readline().split()))
+Nlist.sort()
+def binary_search(target, data):
+
+    start = 0
+    end = len(data) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if data[mid] == target:
+            return mid # 함수를 끝내버린다.
+        elif data[mid] < target:
+            start = mid + 1
+        else:
+            end = mid -1
+
+    return None
+
+
+target = Mlist
+for i in range(M):
+    binary_search(Mlist[i],Nlist)
+    if binary_search(Mlist[i],Nlist) is not None:
+        print(1)
+    else:
+        print(0)
